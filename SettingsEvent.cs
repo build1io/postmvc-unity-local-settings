1e56be623ea12e518a1428bc6d7eb89bba4fb78d
George Markosian <georgemarkosian@gmail.com>
2022-11-23 18:11:38 +0100

    Events improvements.

--- SettingsEvent.cs
+++ SettingsEvent.cs
@@ -5,16 +5,12 @@ namespace Build1.PostMVC.Unity.Settings
 {
     public static class SettingsEvent
     {
-        public static readonly Event            LoadSuccess = new();
-        public static readonly Event<Exception> LoadFail    = new();
-
-        public static readonly Event            UnloadSuccess = new();
-        public static readonly Event<Exception> UnloadFail    = new();
-
-        public static readonly Event<Setting> SettingChanged = new();
-
-        public static readonly Event<Exception> SaveFail = new();
-
-        public static readonly Event Reset = new();
+        public static readonly Event            LoadSuccess    = new(typeof(SettingsEvent), nameof(LoadSuccess));
+        public static readonly Event<Exception> LoadFail       = new(typeof(SettingsEvent), nameof(LoadFail));
+        public static readonly Event            UnloadSuccess  = new(typeof(SettingsEvent), nameof(UnloadSuccess));
+        public static readonly Event<Exception> UnloadFail     = new(typeof(SettingsEvent), nameof(UnloadFail));
+        public static readonly Event<Setting>   SettingChanged = new(typeof(SettingsEvent), nameof(SettingChanged));
+        public static readonly Event<Exception> SaveFail       = new(typeof(SettingsEvent), nameof(SaveFail));
+        public static readonly Event            Reset          = new(typeof(SettingsEvent), nameof(Reset));
     }
 }
